e18d2fdf516ffa880f538c45f92600ef50340777
unfamousk4y <kaoride24@gmail.com>
2026-04-07 04:48:36 +0000

    Use fastembed instead for size

--- backend/services/embedder.py
+++ backend/services/embedder.py
@@ -1,7 +1,7 @@
-from sentence_transformers import SentenceTransformer
+from fastembed import TextEmbedding
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = TextEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
 def get_embedding(text: str) -> list[float]:
-	embedding = model.encode(text)
-	return embedding.tolist()
+    embeddings = list(model.embed([text]))
+    return embeddings[0].tolist()
